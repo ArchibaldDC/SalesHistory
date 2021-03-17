50fafc9663808e885f74a7eed147b1cab0cf668a
Archibald De Coster <archibald@medimarien.be>
2021-03-17 22:49:02 +0100

    code format

--- Script.R
+++ Script.R
@@ -10,17 +10,17 @@ if(!require(tidyverse)) install.packages("tidyverse")
 set.seed(2001)
 
 df <- tibble(the_transaction_id = paste("trans", seq(1,200000), sep = "_"),
-                   but_idr_business_unit = sample(c("Evere", "Wavre", "Anderlecht", "Antwerp", "Charleroi", 
-                                                    "Namur", "Liège"), 200000, replace = T,
-                                                  prob = c(0.35, 0.05, 0.10, 0.15, 0.10, 0.05, 0.20)),
-                   tdt_type_detail = sample(c("sale", "return"), 200000, replace = T, prob = c(0.9, 0.1)),
-                   the_date_transaction = sample(seq(as.Date("2019/01/01"), as.Date("2019/12/31"), by = "day"),
-                                                 200000, replace = T),
-                   sku_idr_sku = as.factor(paste(sample(c("tennis_item", "football_item", "running_item", "biking_item", 
-                                                "other_item"), 200000, replace = T),
-                                       sample(c(1:500), replace = T), sep = "_")),
-                   quantity = as.factor(sample(seq(1:5), 200000, replace = T, 
-                                               prob = c(0.70,0.15, 0.05, 0.03, 0.02))))
+             but_idr_business_unit = sample(c("Evere", "Wavre", "Anderlecht", "Antwerp", "Charleroi", 
+                                              "Namur", "Liège"), 200000, replace = T,
+                                            prob = c(0.35, 0.05, 0.10, 0.15, 0.10, 0.05, 0.20)),
+             tdt_type_detail = sample(c("sale", "return"), 200000, replace = T, prob = c(0.9, 0.1)),
+             the_date_transaction = sample(seq(as.Date("2019/01/01"), as.Date("2019/12/31"), by = "day"),
+                                           200000, replace = T),
+             sku_idr_sku = as.factor(paste(sample(c("tennis_item", "football_item", "running_item", "biking_item", 
+                                                    "other_item"), 200000, replace = T),
+                                           sample(c(1:500), replace = T), sep = "_")),
+             quantity = as.factor(sample(seq(1:5), 200000, replace = T, 
+                                         prob = c(0.70,0.15, 0.05, 0.03, 0.02))))
 
 
   
